b8b7417f7b75a400acf83d122806e73e1a5702c8
Joshua Ward <42013718+Cyph1x@users.noreply.github.com>
2024-10-21 20:55:45 +1000

    Improve config options on normal curves of pursuit

--- normal pursuit curves/main.py
+++ normal pursuit curves/main.py
@@ -10,6 +10,7 @@ debug_figures = False
 debug_figures_starting_circle = True
 debug_figures_position_group_dot = True
 remove_duplicate_lines = False
+
 # Mice
 mice_count = 3 # How many mice
 #step_size = 10 # How much each mouse moves in each step
@@ -19,6 +20,15 @@ step_count = 50 # Number of steps
 rotation = 0 # Rotation offset for mice positioning
 mice_group_count = 5 # Number of mice groups
 
+# Animation
+enable_animation = True# Enable or disable the animation
+animation_duration_ms = 1000 # Duration of the animation in milliseconds
+begin_ms = 0 # Start time of the animation in milliseconds
+infinite_repeat = True # Repeat the animation infinitely
+cross_hatch = False # Cross hatch the lines
+
+# Canvas
+target_resolution = 2000
 # defining the offset required to place the corner of a group at 0,0
 angle =  2 * math.pi / mice_count
 start_x = -distance * math.cos(angle)
@@ -30,19 +40,28 @@ angle =  0 * 2 * math.pi / mice_count
 corner2_x = distance * math.cos(angle)
 corner2_y = distance * math.sin(angle)
 group_distance = math.sqrt((corner1_x - corner2_x) ** 2 + (corner1_y - corner2_y) ** 2)
-
-# Animation
-enable_animation = False# Enable or disable the animation
-animation_duration_ms = 1000 # Duration of the animation in milliseconds
-begin_ms = 0 # Start time of the animation in milliseconds
-duration_ms = 1 # Duration of each step in milliseconds
-infinite_repeat = True # Repeat the animation infinitely
-fill = 'freeze' # Fill the final state of the animation
-
-# Canvas
-#resolution = 5000
 resolution = group_distance * 2
-canvas = draw.Drawing(resolution, resolution, origin="center")
+
+#scale to the target resolution
+if 'target_resolution' in locals():
+    scale = target_resolution / resolution
+    resolution *= scale
+    distance *= scale
+    group_distance *= scale
+    start_x *= scale
+    start_y *= scale
+
+line_width = 0.001*resolution
+
+canvas = draw.Drawing(resolution, resolution, origin="center",
+        animation_config=draw.types.SyncedAnimationConfig(
+            # Animation configuration
+            duration=animation_duration_ms/1000,  # Seconds
+            #show_playback_progress=True,
+            #show_playback_controls=True)
+            )
+        )
+
 
 #set the background color
 canvas.append(draw.Rectangle(-resolution/2, -resolution/2, resolution, resolution, fill='None'))
@@ -93,69 +112,110 @@ class Mouse:
         self.x = self.new_x
         self.y = self.new_y
 
-def generate_mice_lines(n: int, n_steps: int, distance: float, rotation: float, reverse: bool) -> Group:
-    new_mice = []
-    g = draw.Group()
-    if debug_figures and debug_figures_starting_circle:
-        # Define the circle radius
-        g.append(draw.Circle(0, 0, distance, fill='none', stroke='black'))
-    for i in range(n):
-        angle = rotation + i * 2 * math.pi / n
-        #angle = random.uniform(0, 2 * math.pi)
-        x = 0 + distance * math.cos(angle)
-        y = 0 + distance * math.sin(angle)
-        new_mice.append(Mouse(x, y))
-    if reverse:
-        new_mice.reverse()
-    for i in range(n):
-        print(f"Mouse {i}: ({new_mice[i].x:.2f}, {new_mice[i].y:.2f})")
-        new_mice[i].set_target(new_mice[(i + 1) % n])
+class MouseLines:
 
-    for i in range(n_steps):
-        for mouse in new_mice:
-            step_size = mouse.get_target_distance() * target_distance_scale
-            mouse.move(step_size)
+    def __init__(self,n: int, n_steps: int, distance: float, rotation: float, reverse: bool, colour: str, line_width: float, opacity=0.5,begin_ms: float = begin_ms, repeat: bool=infinite_repeat):
+        self.n = n
+        self.n_steps = n_steps
+        self.distance = distance
+        self.rotation = rotation
+        self.reverse = reverse
+        self.colour = colour
+        self.line_width = line_width
+        self.opacity = opacity
+        self.begin_ms = begin_ms
+        self.repeat = repeat
+        self.new_mice = []
 
-        mouse_lines = []
-        target_lines = []
-        for mouse in new_mice:
-            x1, y1 = mouse.get_position()
-            x2, y2 = mouse.get_target().get_position()
-            x3, y3 = mouse.new_x, mouse.new_y
-            x4, y4 = mouse.get_target().new_x, mouse.get_target().new_y
-            mouse_lines.extend([x1, y1, x2, y2])
-            target_lines.extend([x3, y3, x4, y4])
+    def generate_mice_lines(self,g: draw.Group=None,t=0):
+        new_mice = []
+        if g is None:
+            g = draw.Group()
+        if debug_figures and debug_figures_starting_circle:
+            # Define the circle radius
+            g.append(draw.Circle(0, 0, distance, fill='none', stroke='black'))
+        for i in range(self.n):
+            angle = self.rotation + i * 2 * math.pi / self.n
+            # angle = random.uniform(0, 2 * math.pi)
+            x = 0 + distance * math.cos(angle)
+            y = 0 + distance * math.sin(angle)
+            new_mice.append(Mouse(x, y))
+        if self.reverse:
+            new_mice.reverse()
+        for i in range(self.n):
+            print(f"Mouse {i}: ({new_mice[i].x:.2f}, {new_mice[i].y:.2f})")
+            new_mice[i].set_target(new_mice[(i + 1) % self.n])
 
-        mouse_lines_background = draw.Lines(*mouse_lines, close=True, fill='none', stroke='black', stroke_width=2,opacity=0.5)
-        target_lines_background = draw.Lines(*target_lines, close=True, fill='none', stroke='black', stroke_width=2,opacity=0.5)
-        mouse_lines_foreground = draw.Lines(*mouse_lines, close=True, fill='none', stroke='white', stroke_width=2,opacity=0.5)
-        target_lines_foreground = draw.Lines(*target_lines, close=True, fill='none', stroke='white', stroke_width=2,opacity=0.5)
+        for i in range(self.n_steps):
+            for mouse in new_mice:
+                step_size = mouse.get_target_distance() * target_distance_scale
+                mouse.move(step_size)
 
-        if reverse:
-            if enable_animation:
-                mouse_lines_background.append_anim(draw.Animate(attributeName='d', to=target_lines_background.args["d"], begin=f"{begin_ms}ms", dur=f"{animation_duration_ms}ms",repeatCount='indefinite'))
-                mouse_lines_foreground.append_anim(draw.Animate(attributeName='d', to=target_lines_foreground.args["d"], begin=f"{begin_ms}ms", dur=f"{animation_duration_ms}ms",repeatCount='indefinite'))
-                #mouse_lines.add_key_frame(0, d=target_lines.args["d"])
-                #mouse_lines.add_key_frame(animation_duration_ms/1000, d=mouse_lines.args["d"])
-            lines_foreground=mouse_lines_foreground
-            lines_background=target_lines_background
-        else:
-            if enable_animation:
-                target_lines_background.append_anim(draw.Animate(attributeName='d', to=mouse_lines_background.args["d"], begin=f"{begin_ms}ms", dur=f"{animation_duration_ms}ms",repeatCount='indefinite'))
-                target_lines_foreground.append_anim(draw.Animate(attributeName='d', to=mouse_lines_foreground.args["d"], begin=f"{begin_ms}ms", dur=f"{animation_duration_ms}ms",repeatCount='indefinite'))
-                #target_lines.add_key_frame(0, d=mouse_lines.args["d"])
-                #target_lines.add_key_frame(animation_duration_ms/1000, d=target_lines.args["d"])
-            lines_foreground=target_lines_foreground
-            lines_background=mouse_lines_background
+            mouse_lines = []
+            target_lines = []
+            for mouse in new_mice:
+                x1, y1 = mouse.get_position()
+                x2, y2 = mouse.get_target().get_position()
+                x3, y3 = mouse.new_x, mouse.new_y
+                x4, y4 = mouse.get_target().new_x, mouse.get_target().new_y
+                #interpolate the x1,y1,x2,y2,x3,y3,x4,y4
+                x1,y1 = x1 + (x3-x1)*t, y1 + (y3-y1)*t
+                x2,y2 = x2 + (x4-x2)*t, y2 + (y4-y2)*t
 
-        g.append(lines_background)
-        g.append(lines_foreground)
-        for mouse in new_mice:
-            mouse.update_position()
-    return g
+                mouse_lines.extend([x1, y1, x2, y2])
+                target_lines.extend([x3, y3, x4, y4])
 
-forward_animation = generate_mice_lines(mice_count, step_count, distance, rotation,reverse=False)
-reverse_animation = generate_mice_lines(mice_count, step_count, distance, rotation,reverse=True)
+            mouse_lines = draw.Lines(*mouse_lines, close=True, fill='none', stroke=self.colour, stroke_width=line_width,
+                                     opacity=self.opacity)
+            target_lines = draw.Lines(*target_lines, close=True, fill='none', stroke=self.colour, stroke_width=line_width,
+                                      opacity=self.opacity)
+
+            repeat_count = 'indefinite' if self.repeat else 1
+
+            if self.reverse:
+                if enable_animation:
+                    mouse_lines.append_anim(
+                        draw.Animate(attributeName='d', to=target_lines.args["d"], begin=f"{self.begin_ms}ms",
+                                     dur=f"{animation_duration_ms}ms", repeatCount=repeat_count))
+                lines = mouse_lines
+            else:
+                if enable_animation:
+                    target_lines.append_anim(
+                        draw.Animate(attributeName='d', to=mouse_lines.args["d"], begin=f"{self.begin_ms}ms",
+                                     dur=f"{animation_duration_ms}ms", repeatCount=repeat_count))
+                lines = target_lines
+
+            g.append(lines)
+            for mouse in new_mice:
+                mouse.update_position()
+        return g
+
+class MouseGroup:
+    def __init__(self):
+        self.mice: List[MouseLines] = []
+
+    def AddMouseLines(self, mouse_lines: MouseLines):
+        self.mice.append(mouse_lines)
+
+    def generate_mice_lines(self,g: draw.Group = None,t: float = 0):
+        if g is None:
+            g = draw.Group()
+        for mouse in self.mice:
+            mouse.generate_mice_lines(g,t)
+        return g
+
+forward_animation_group = MouseGroup()
+forward_animation = MouseLines(mice_count, step_count, distance, rotation,reverse=False,colour='black',line_width=line_width,opacity=0.5,begin_ms=begin_ms)
+forward_animation_group.AddMouseLines(forward_animation)
+forward_animation = MouseLines(mice_count, step_count, distance, rotation,reverse=False,colour='white',line_width=line_width,opacity=0.5,begin_ms=begin_ms)
+forward_animation_group.AddMouseLines(forward_animation)
+
+reverse_animation_group = MouseGroup()
+reverse_animation = MouseLines(mice_count, step_count, distance, rotation,reverse=True,colour='black',line_width=line_width,opacity=0.5,begin_ms=begin_ms)
+reverse_animation_group.AddMouseLines(reverse_animation)
+reverse_animation = MouseLines(mice_count, step_count, distance, rotation,reverse=True,colour='white',line_width=line_width,opacity=0.5,begin_ms=begin_ms)
+reverse_animation_group.AddMouseLines(reverse_animation)
+
 
 group_positions = [(start_x, start_y, rotation)]
 
@@ -238,16 +298,62 @@ for i, group_positions in enumerate(group_position_layers):
         else:
             forward_positions.add(group_position)
     reverse = not reverse
+
+
+forward_animation_group_generated = forward_animation_group.generate_mice_lines()
+reverse_animation_group_generated = reverse_animation_group.generate_mice_lines()
 for x, y, rotation in forward_positions:
-    canvas.append(draw.Use(forward_animation, x=x, y=y, transform=f"rotate({math.degrees(rotation)} {x} {y})"))
+    canvas.append(draw.Use(forward_animation_group_generated, x=x, y=y, transform=f"rotate({math.degrees(rotation)} {x} {y})"))
+    if cross_hatch:
+        canvas.append(
+            draw.Use(reverse_animation_group_generated, x=x, y=y, transform=f"rotate({math.degrees(rotation)} {x} {y})"))
 for x, y, rotation in reverse_positions:
-    canvas.append(draw.Use(reverse_animation, x=x, y=y, transform=f"rotate({math.degrees(rotation)} {x} {y})"))
+    canvas.append(draw.Use(reverse_animation_group_generated, x=x, y=y, transform=f"rotate({math.degrees(rotation)} {x} {y})"))
+    if cross_hatch:
+        canvas.append(
+            draw.Use(forward_animation_group_generated, x=x, y=y, transform=f"rotate({math.degrees(rotation)} {x} {y})"))
 
 
 #canvas.append(draw.Circle(0, 0, 50, fill='red'))
 #canvas.append(draw.Circle(0, 0, group_distance, fill='none', stroke='red'))
 
 canvas.save_svg('mice.svg')
+
+fps = 30
+duration = animation_duration_ms / 1000
+time_step = 1 / fps
+steps = int(duration / time_step)
+def gen_frame(t):
+    t = t / duration
+    t = t - int(t)
+    print(t)
+    canvas = draw.Drawing(resolution, resolution, origin="center")
+    canvas.append(draw.Rectangle(-resolution / 2, -resolution / 2, resolution, resolution, fill='None'))
+    forward_animation_group_generated = forward_animation_group.generate_mice_lines(t=t)
+    reverse_animation_group_generated = reverse_animation_group.generate_mice_lines(t=t)
+    for x, y, rotation in forward_positions:
+        canvas.append(draw.Use(forward_animation_group_generated, x=x, y=y,
+                               transform=f"rotate({math.degrees(rotation)} {x} {y})"))
+        if cross_hatch:
+            canvas.append(
+                draw.Use(reverse_animation_group_generated, x=x, y=y,
+                         transform=f"rotate({math.degrees(rotation)} {x} {y})"))
+    for x, y, rotation in reverse_positions:
+        canvas.append(draw.Use(reverse_animation_group_generated, x=x, y=y,
+                               transform=f"rotate({math.degrees(rotation)} {x} {y})"))
+        if cross_hatch:
+            canvas.append(
+                draw.Use(forward_animation_group_generated, x=x, y=y,
+                         transform=f"rotate({math.degrees(rotation)} {x} {y})"))
+    print(f"Saving frame {i}")
+    #canvas.save_png(f"mice_{i:04d}.png")
+    return canvas
+
+#with draw.frame_animate_video('mice.mp4', gen_frame,duration=0.05) as frame:
+    #for i in range(steps):
+        #frame.draw_frame(i)
+
+#canvas.as_mp4('mice.mp4',fps=10,verbose=True)
 #canvas.save_html('mice.html')
 
 
